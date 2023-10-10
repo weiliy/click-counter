1b9bfa465d1991ccbe082fbd8e47ccc0e412dac8
Weili Y <yi@weiliy.net>
2023-10-10 16:50:53 +0800

    Initial Commit

---
+++ Click Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Click Counter
+//
+//  Created by Weili Yi on 2023/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
